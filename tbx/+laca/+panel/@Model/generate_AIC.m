ccea9746b7085e1c7f5f6eff6d694b0ddb0eb742
fh9g12 <fintanhealy@live.co.uk>
2023-03-25 18:10:41 +0000

    remove redudent files

--- tbx/+laca/+panel/@Model/generate_AIC.m
+++
@@ -1,6 +0,0 @@
-function obj = generate_AIC(obj)
-% compute influence of wing panels
-obj.AIC = laca.panel.vlm_C_code('generate_AIC',obj.Rings,...
-    obj.Collocation,obj.Normal,obj.TERings,obj.TEidx);
-end
-
